02de6bc8cffd1b71d93d81025739830d56675ca3
Jethro <ttppoo12144@gmail.com>
2024-07-29 14:15:21 +1000

    update index style

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import '@/assets/styles/tailwind.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const countButton = document.getElementById('countButton');
+  const countSpan = document.getElementById('count');
+  let count = 0;
+
+  countButton.addEventListener('click', () => {
+    count += 1;
+    countSpan.textContent = count;
+  });
+});
